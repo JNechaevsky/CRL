d862faa488ebb7b355963ed84299898507480b7f
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2018-03-04 21:24:06 +0300

    Got Code::Blocks working

--- codeblocks/config.h
+++ codeblocks/config.h
@@ -9,26 +9,22 @@
 #define PACKAGE_NAME "Chocorenderlimits"
 
 /* Define to the full name and version of this package. */
-<<<<<<< HEAD
-#define PACKAGE_STRING "Chocorenderlimits 2.3.0"
-=======
-#define PACKAGE_STRING "Chocolate Doom 3.0.0-beta1"
->>>>>>> 7bd39542f9c056acda71d1fb3dd976b08501c468
+#define PACKAGE_STRING "Chocorenderlimits 3.0.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "chocorenderlimits"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.0.0-beta1"
+#define PACKAGE_VERSION "3.0.0"
 
 /* Change this when you create your awesome forked version */
-#define PROGRAM_PREFIX "chocorenderlimits-"
+/* #define PROGRAM_PREFIX "chocorenderlimits-"             */
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "3.0.0-beta1"
+#define VERSION "3.0.0"
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
