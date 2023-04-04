2862db6f45f99cbacbbf2d86e4479e6471125807
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2023-04-04 16:01:15 +0300

    Windows: use modern icons and system dialog box

--- src/i_system.c
+++ src/i_system.c
@@ -259,6 +259,10 @@ static boolean already_quitting = false;
 void I_Error (char *error, ...)
 {
     char msgbuf[512];
+#ifdef _WIN32
+    wchar_t win_error_message[1024];  // [JN] UTF-8 retranslation of error message
+    wchar_t win_error_title[128];     // [JN] UTF-8 retranslation of window title
+#endif
     va_list argptr;
     atexit_listentry_t *entry;
     boolean exit_gui_popup;
@@ -308,8 +312,15 @@ void I_Error (char *error, ...)
     // therefore be unable to otherwise see the message).
     if (exit_gui_popup && !I_ConsoleStdout())
     {
+#ifdef _WIN32
+        // [JN] On Windows OS use system, nicer diaalog box.
+        MultiByteToWideChar(CP_UTF8, 0, msgbuf, -1, win_error_message, 1024);
+        MultiByteToWideChar(CP_UTF8, 0, PACKAGE_STRING, -1, win_error_title, 128);
+        MessageBoxW(NULL, win_error_message, win_error_title, MB_ICONSTOP);
+#else
         SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,
                                  PACKAGE_STRING, msgbuf, NULL);
+#endif
     }
 
     // abort();
