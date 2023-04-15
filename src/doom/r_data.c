528e7505d4a1bf0feb330d7db313e8fdcc6d6555
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2023-04-15 12:23:41 +0300

    Don't print double brackets on Linux

--- src/doom/r_data.c
+++ src/doom/r_data.c
@@ -478,11 +478,6 @@ void R_InitTextures (void)
 
     int*		directory;
     
-    int			temp1;
-    int			temp2;
-    int			temp3;
-
-    
     // Load the patch names from pnames.lmp.
     name[8] = 0;
     names = W_CacheLumpName (DEH_String("PNAMES"), PU_STATIC);
@@ -527,25 +522,6 @@ void R_InitTextures (void)
     texturewidthmask = Z_Malloc (numtextures * sizeof(*texturewidthmask), PU_STATIC, 0);
     textureheight = Z_Malloc (numtextures * sizeof(*textureheight), PU_STATIC, 0);
     
-    //	Really complex printing shit...
-    temp1 = W_GetNumForName (DEH_String("S_START"));  // P_???????
-    temp2 = W_GetNumForName (DEH_String("S_END")) - 1;
-    temp3 = ((temp2-temp1+63)/64) + ((numtextures+63)/64);
-
-    // If stdout is a real console, use the classic vanilla "filling
-    // up the box" effect, which uses backspace to "step back" inside
-    // the box.  If stdout is a file, don't draw the box.
-
-    if (I_ConsoleStdout())
-    {
-        printf("[");
-        for (i = 0; i < temp3 + 9; i++)
-            printf(" ");
-        printf("]");
-        for (i = 0; i < temp3 + 10; i++)
-            printf("\b");
-    }
-	
     for (i=0 ; i<numtextures ; i++, directory++)
     {
 	if (!(i&63))
