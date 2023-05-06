67890ab22effc5017c32ecad2a83189fe7452368
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2023-05-06 12:26:21 +0300

    Heretic: erm, scroll right is `99`, not `85`

--- src/heretic/p_spec.c
+++ src/heretic/p_spec.c
@@ -1071,7 +1071,7 @@ void R_InterpolateTextureOffsets (void)
 				side->textureoffset = side->basetextureoffset + fractionaltic;
 			}
 			else
-			if (line->special == 85)
+			if (line->special == 99)
 			{
 				side->textureoffset = side->basetextureoffset - fractionaltic;
 			}
