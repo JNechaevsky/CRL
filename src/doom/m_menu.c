22b355b98d7864f2bd5eaa5a34379a2b426d3baa
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2023-10-22 20:08:53 +0300

    Fix remaining pedantic warnings

--- src/doom/m_menu.c
+++ src/doom/m_menu.c
@@ -167,7 +167,7 @@ typedef struct menu_s
     short		numitems;	// # of menu items
     struct menu_s*	prevMenu;	// previous menu
     menuitem_t*		menuitems;	// menu items
-    void		(*routine)();	// draw routine
+    void		(*routine)(void);	// draw routine
     short		x;
     short		y;		// x,y of menu
     short		lastOn;		// last item user was on in menu
@@ -5246,7 +5246,7 @@ static void M_ConfirmDeleteGameResponse (int key)
 	}
 }
 
-void M_ConfirmDeleteGame ()
+void M_ConfirmDeleteGame (void)
 {
 	savegwarning =
 	M_StringJoin("delete savegame\n\n", "\"", savegamestrings[itemOn], "\"", " ?\n\n",
