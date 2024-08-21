d1189db2af3a02d27f5a5c90b725abf9af37a237
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2024-08-21 22:44:51 +0300

    Remember cursor position in Sound Volume menu

--- src/doom/m_menu.c
+++ src/doom/m_menu.c
@@ -4986,7 +4986,7 @@ boolean M_Responder (event_t* ev)
         {
 	    M_StartControlPanel ();
 	    currentMenu = &SoundDef;
-	    itemOn = sfx_vol;
+	    itemOn = currentMenu->lastOn;
 	    S_StartSound(NULL,sfx_swtchn);
 	    return true;
 	}
