73d09557ccf96bdd9d89f74ce88d9a4dc6d571f0
pvictress <julia.nechaevskaya@live.com>
2025-01-04 20:26:54 +0300

    Move on to 2025

--- src/doom/deh_bexstr.c
+++ src/doom/deh_bexstr.c
@@ -2,7 +2,7 @@
 // Copyright(C) 2005-2014 Simon Howard
 // Copyright(C) 2014 Fabian Greffrath
 // Copyright(C) 2011-2017 RestlessRodent
-// Copyright(C) 2018-2024 Julia Nechaevskaya
+// Copyright(C) 2018-2025 Julia Nechaevskaya
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
