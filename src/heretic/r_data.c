55498a502c2d80b6493330b82855bb9c3a298388
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2026-08-06 00:26:13 +0300

    Fix absurd texture name in error message

--- src/heretic/r_data.c
+++ src/heretic/r_data.c
@@ -254,8 +254,12 @@ static void R_GenerateLookup(int texnum)
     {
         if (!patchcount[x])
         {
-            printf("R_GenerateLookup: column without a patch (%s)\n",
-                   texture->name);
+            char badtexture[16];
+
+            // [crispy] fix absurd texture name in error message
+            // [JN] CRL - clarify that we are handing texture here.
+            sprintf (badtexture, "\"%.8s\"", texture->name);
+            CRL_printf(M_StringJoin("\nR_GenerateLookup: missing a patch in the texture ", badtexture, NULL), false);
             return;
         }
 //                      I_Error ("R_GenerateLookup: column without a patch");
